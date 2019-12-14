41ce4a8e407f4a06f88908b79415c0f580257a0a
hongtor78 <hongtor76@gmail.com>
2019-12-14 10:15:15 +0800

    Add home navigation

---
+++ about.php
@@ -0,0 +1,4 @@
+<a href="index.php.php">Home</a>
+<a href="about.php">About</a>
+<hr/>
+<h1> About </h1>
--- index.php
+++ index.php
@@ -1,9 +1,6 @@
+<a href="index.php.php">Home</a>
 <a href="about.php">About</a>
-
+<hr/>
 <?php
-
-
 echo "Hello world";
-
-
 ?>
